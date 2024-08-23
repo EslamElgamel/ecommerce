39698aa7bb3b679c3831e9f1dd512196a49290c2
Eslam <islam.mostafa893@gmail.com>
2024-08-23 18:28:32 +0300

    webhook-checkout-problem

--- server.js
+++ server.js
@@ -17,29 +17,18 @@ const dbConnection = require('./config/database');
 const mountRoutes = require('./routes')
 const {webhookCheckout} = require('./services/orderService')
 
-// const categoryRoute = require('./routes/categoryRoute');
-// const subCategoryRoute = require('./routes/subCategoryRoute');
-// const brandRoute = require('./routes/brandRoute');
-// const productRoute = require('./routes/productRoute');
-// const userRoute = require('./routes/userRoute')
-// const authRoute = require('./routes/authRoute')
-// const reviewRoute = require('./routes/reviewRoute')
-// const wishlistRoute = require('./routes/wishlistRoute')
-// const addressRoute = require('./routes/addressRoute')
-// const couponRoute = require('./routes/couponRoute')
-
 // Connect with db
 dbConnection();
 
 // express app
-const app = express(); 
+const app = express();
 
-// Enable other domains to access you application
-app.use(cors())
-app.options('*', cors())
+// Enable other domains to access your application
+app.use(cors());
+app.options('*', cors());
 
 // compress all responses
-app.use(compression())
+app.use(compression());
 
 // Checkout webhook
 app.post(
@@ -50,28 +39,15 @@ app.post(
 
 // Middlewares
 app.use(express.json());
-app.use(express.static(path.join(__dirname,'uploads')))
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
   console.log(`mode: ${process.env.NODE_ENV}`);
 }
 
-// console.log(crypto.randomBytes(32).toString('hex'))
-
-
 // Mount Routes
-mountRoutes(app)
-// app.use('/api/v1/categories', categoryRoute);
-// app.use('/api/v1/subcategories', subCategoryRoute);
-// app.use('/api/v1/brands', brandRoute);
-// app.use('/api/v1/products', productRoute);
-// app.use('/api/v1/users', userRoute);
-// app.use('/api/v1/auth', authRoute);
-// app.use('/api/v1/reviews', reviewRoute);
-// app.use('/api/v1/wishlists', wishlistRoute);
-// app.use('/api/v1/address', addressRoute);
-// app.use('/api/v1/coupons', couponRoute);
+mountRoutes(app);
 
 app.all('*', (req, res, next) => {
   next(new ApiError(`Can't find this route: ${req.originalUrl}`, 400));
